f9c5830e9e8345159f41c3693154d81de6f3c8d4
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-02-01 15:41:33 -0300

    feat: update styles on notion page

--- src/Notion/index.tsx
+++ src/Notion/index.tsx
@@ -1,3 +1,8 @@
+const borderStyle = {
+  border: "1px solid #000000",
+  backgroundColor: "#d1d0d0b5",
+};
+
 export const Notion = () => {
   return (
     <div
@@ -15,35 +20,37 @@ export const Notion = () => {
         em testes.
       </h2>
 
-      <table style={{ border: "1px solid #000000" }}>
+      <table style={borderStyle}>
         <tr>
-          <th style={{ border: "1px solid #000000" }}>Get</th>
-          <th style={{ border: "1px solid #000000" }}>Query</th>
-          <th style={{ border: "1px solid #000000" }}>Find</th>
+          <th style={borderStyle}>Get</th>
+          <th style={borderStyle}>Query</th>
+          <th style={borderStyle}>Find</th>
         </tr>
         <tr>
-          <td>
+          <td style={borderStyle}>
             retorna o nó correspondente, no entanto, lançará um erro se várias
             correspondências ou nenhuma correspondência for encontrada.
           </td>
-          <td>
+          <td style={borderStyle}>
             retorna o nó correspondente se uma correspondência for encontrada e
             nulo se nenhuma correspondência for encontrada; no entanto, lançará
             um erro se várias correspondências forem encontradas.
           </td>
-          <td>
+          <td style={borderStyle}>
             retorna uma promessa que retorna o nó correspondente, no entanto,
             lançará um erro se várias correspondências ou nenhuma
             correspondência for encontrada.
           </td>
         </tr>
         <tr>
-          <td>Use esses métodos se você espera que o elemento</td>
-          <td>
+          <td style={borderStyle}>
+            Use esses métodos se você espera que o elemento
+          </td>
+          <td style={borderStyle}>
             Use esses métodos se estiver procurando confirmar a presença de um
             elemento/elementos.
           </td>
-          <td>
+          <td style={borderStyle}>
             Use esses métodos se o elemento/elementos que estão sendo
             consultados puderem ser exibidos de forma assíncrona (por exemplo,
             se for esperado que seu elemento seja exibido apenas após um evento
