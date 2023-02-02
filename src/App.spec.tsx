35eb17f240cd26872d3827d3b38e3a60e1ef4c5b
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-02-02 12:06:14 -0300

    feat: test with function on button

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("App component", () => {
@@ -8,5 +8,15 @@ describe("App component", () => {
     expect(screen.getByText("Otávio")).toBeInTheDocument();
     expect(screen.getByText("Matheus")).toBeInTheDocument();
     expect(screen.getByText("Jhony")).toBeInTheDocument();
+  });
+
+  it("Should be able to add a new item to the list", () => {
+    render(<App />);
+
+    const addButton = screen.getByText("Adicionar");
+
+    fireEvent.click(addButton);
+
+    expect(screen.getByText("Novo")).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,27 @@ import { useState } from "react";
 import { Notion } from "./Notion";
 
 const App = () => {
-  const [listNames, setListNames] = useState<string[]>([
-    "Otávio",
-    "Matheus",
-    "Jhony",
-  ]);
+  const [listNames, setListNames] = useState(["Otávio", "Matheus", "Jhony"]);
 
-  const handleUpdateListName = () => {
-    setListNames([...listNames, "Novo nome"]);
-  };
+  function handleUpdateListName() {
+    setListNames((state) => [...state, "Novo"]);
+  }
   return (
     <>
       <header style={{ textAlign: "center" }}>
         <h1>Lista de nomes</h1>
 
-        {listNames.map((name) => (
-          <li>{name}</li>
-        ))}
+        <ul>
+          {listNames.map((name) => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
 
         <button onClick={handleUpdateListName} style={{ marginTop: "1rem" }}>
-          Clique aqui para adicionar um novo nome
+          Adicionar
         </button>
       </header>
+
       <body>
         <Notion />
       </body>
